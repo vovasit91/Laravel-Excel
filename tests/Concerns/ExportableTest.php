eabe022a0ca55042266097fd5bb00571c1989c7e
Patrick Brouwers <patrickbrouwers@users.noreply.github.com>
2021-05-27 19:36:46 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Concerns/ExportableTest.php
+++ tests/Concerns/ExportableTest.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Excel;
 use Maatwebsite\Excel\Exceptions\NoFilePathGivenException;
-use Maatwebsite\Excel\Tests\Data\Stubs\Database\Group;
-use Maatwebsite\Excel\Tests\Data\Stubs\Database\User;
 use Maatwebsite\Excel\Tests\Data\Stubs\EmptyExport;
 use Maatwebsite\Excel\Tests\Data\Stubs\UsersExport;
 use Maatwebsite\Excel\Tests\TestCase;
